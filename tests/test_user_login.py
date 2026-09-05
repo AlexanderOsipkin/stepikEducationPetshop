5fe3bda9c796f4db3892bfdd0985f33c65865b29
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2026-09-05 14:34:54 +0300

    code refactoring and add new method for popup

--- tests/test_user_login.py
+++ tests/test_user_login.py
@@ -7,8 +7,6 @@ def test_user_login():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     options.add_argument("--guest")
-    options.add_argument("--disable-popup-blocking")
-    options.add_argument("--disable-notifications")
     driver = webdriver.Chrome(options=options)
 
     login = LoginPage(driver)
